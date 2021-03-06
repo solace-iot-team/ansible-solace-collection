2b948f840eccc9870866cbff291d8e51eee02113
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-03-06 16:36:35 +0000

    integration workflow

--- tests/roles/azure/create.az.vm.sh
+++ tests/roles/azure/create.az.vm.sh
@@ -64,11 +64,6 @@ echo " >>> Creating azure vm ..."
   ls -la ~/.ssh/id_rsa*
   cat ~/.ssh/id_rsa; if [[ $? != 0 ]]; then echo " >>> ERROR: vm private key"; exit 1; fi
   cat ~/.ssh/id_rsa.pub; echo ""; if [[ $? != 0 ]]; then echo " >>> ERROR: vm pub key"; exit 1; fi
-  vmPublicIpAddress=$(cat $outputInfoFile | jq -r '.publicIpAddress')
-  ssh_test=$(ssh "$vmAdminUsr@$vmPublicIpAddress" "bash --version")
-  if [[ $? != 0 || -z "$ssh_test" ]]; then echo " >>> ERROR: ssh into vm"; exit 1; fi
-  echo "ssh_test=$ssh_test"
-  exit 1
 echo " >>> Success."
 
 echo " >>> Opening semp port on azure vm ..."
@@ -79,6 +74,21 @@ echo " >>> Opening semp port on azure vm ..."
     --verbose
   if [[ $? != 0 ]]; then echo " >>> ERROR: opening semp port on azure vm"; exit 1; fi
 echo " >>> Success."
+
+echo " >>> Test ssh to vm ..."
+  vmPublicIpAddress=$(cat $outputInfoFile | jq -r '.publicIpAddress')
+  tries=0; max=10; code=1;
+  while [[ $tries -lt $max && $code -gt 0 ]]; do
+    ((tries++))
+    ssh_test=$(ssh "$vmAdminUsr@$vmPublicIpAddress" "bash --version")
+    code=$?
+    echo "ssh_test=$ssh_test"
+    if [[ $code != 0 ]]; then; echo "code=$code && tries=$tries, sleep 1m"; sleep 1m; fi
+  done
+  if [[ $code != 0 || -z "$ssh_test" ]]; then echo " >>> ERROR: ssh into vm"; exit 1; fi
+echo " >>> Success."
+
+exit 1
 
 echo " >>> Calling Bootstrap vm ..."
   vmPublicIpAddress=$(cat $outputInfoFile | jq -r '.publicIpAddress')
