18d3fc3efe5f2b6f617be085014671c8e3056c1e
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-05-13 15:19:44 +0100

    mtn-132

--- test-runner/_run.integration.sh
+++ test-runner/_run.integration.sh
@@ -28,10 +28,11 @@ source $PROJECT_HOME/.lib/functions.sh
   # "solace/solace-pubsub-standard:9.3.1.28"
   # "solace/solace-pubsub-standard:9.5.0.30"
   # "solace/solace-pubsub-standard:9.7.0.29"
+  # "solace/solace-pubsub-standard:9.8.0.12"
 
   brokerDockerImages=(
     "solace/solace-pubsub-standard:9.6.0.46"
-    "solace/solace-pubsub-standard:9.8.0.12"
+    "solace/solace-pubsub-standard:9.9.0.23"
     "solace/solace-pubsub-standard:latest"
   )
   export AZURE_BROKER_PROJECT_NAME="asct-tr-broker"
