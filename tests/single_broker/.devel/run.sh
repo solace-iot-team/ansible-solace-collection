7f9363d3ddff0e1ce782805c00553be47250faf6
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-05-19 12:50:43 +0100

    test with 3.8

--- tests/single_broker/.devel/run.sh
+++ tests/single_broker/.devel/run.sh
@@ -45,10 +45,10 @@ export ANSIBLE_SOLACE_TESTS="${ansibleSolaceTests[*]}"
   export BROKER_DOCKER_COMPOSE_FILE="$projectHome/test-runner/files/PubSubStandard_singleNode.yml"
   export INVENTORY_FILE=$LOCAL_BROKER_INVENTORY_FILE
 
-# solace cloud broker
-  export BROKER_TYPE="solace_cloud"
-  export INVENTORY_FILE="$projectHome/test-runner/files/solace-cloud-account.inventory.yml"
-  export SOLACE_CLOUD_API_TOKEN=$SOLACE_CLOUD_API_TOKEN_ALL_PERMISSIONS
+# # solace cloud broker
+#   export BROKER_TYPE="solace_cloud"
+#   export INVENTORY_FILE="$projectHome/test-runner/files/solace-cloud-account.inventory.yml"
+#   export SOLACE_CLOUD_API_TOKEN=$SOLACE_CLOUD_API_TOKEN_ALL_PERMISSIONS
 
 export CLEAN_WORKING_DIR=False
 
